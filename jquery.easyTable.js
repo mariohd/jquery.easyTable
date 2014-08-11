a2f4a56da6221904c3b028551a9c366efb3bf7a8
Thiago Ponte <thiagoponte@gmail.com>
2014-08-11 18:22:04 -0300

    Passed the code through JSLint

--- jquery.easyTable.js
+++ jquery.easyTable.js
@@ -1,81 +1,59 @@
-;(function($) {
-  $.fn.easyTable = function( action , options ) {
-    var opts = $.extend( {}, $.fn.easyTable.defaults, options );
 
-    var $this = $(this);
-    var $scrollableFather = $this.parents().filter( function() {
-                  return $(this).css('overflow') == 'auto';
-               });
-    var $headerFixed = $("#" + $this.attr('id') + '-fixedClone');
+(function ($) {
+    "use strict";
+    $.fn.easyTable = function( action , options ) {
+    var opts = $.extend( {}, $.fn.easyTable.defaults, options ),
+    $this = $(this),
+    $scrollableFather = $this.parents().filter( function() {
+        return $(this).css('overflow') === 'auto';
+    }),
+    $headerFixed = $("#" + $this.attr('id') + '-fixedClone'),
+    $tr;
 
-      switch ( action ) {
+    function _getScrollbarWidth() {
+        if ( $this.height() > $scrollableFather.height() ) {
+            var outer = document.createElement("div"),
+                widthNoScroll,
+                inner,
+                widthWithScroll;
+            outer.style.visibility = "hidden";
+            outer.style.width = "100px";
+            outer.style.msOverflowStyle = "scrollbar";
 
-        case 'fixedHead':
-          if ( ! _isFixedHeaded() ){
-            _fixedHeader();
-            $(window).resize(_resizeFixedHeader);
-          }
-          break;
+            document.body.appendChild(outer);
 
-        case 'undoFixedHead':
-          if ( _isFixedHeaded() ){
-            _removeFixedHeader();
-          }
-          break;
+            widthNoScroll = outer.offsetWidth;
 
-        case 'addRow':
-          if (typeof opts.beforeAdd == 'function'){
-            opts.beforeAdd.call( $this );
-          }
+            outer.style.overflow = "scroll";
 
-          _addRow();
-          if ( _isFixedHeaded() ) {
-            _resizeFixedHeader();
-          }
+            inner = document.createElement("div");
+            inner.style.width = "100%";
+            outer.appendChild(inner);
 
-          if (typeof opts.afterAdd == 'function'){
-            opts.afterAdd.call( $this, $tr );
-          }
+            widthWithScroll = inner.offsetWidth;
 
-          break;
+            outer.parentNode.removeChild(outer);
 
-        case 'removeRow':
-          if (typeof opts.beforeDelete == 'function'){
-            opts.beforeDelete.call( $this );
-          }
+            return widthNoScroll - widthWithScroll;
+        }
+        return 0;
+    }
 
-          opts.indexes.sort().reverse();
-          opts.indexes.some( function ( value ) {
-            $this.find('tbody tr')[value].remove();
-          });
+    function _resizeFixedHeader() {
 
-          if ( _isFixedHeaded() ) {
-            _resizeFixedHeader();
-          }
+        if ( $this.find("th").length > 0 ) {
+            $headerFixed.find("th").each(function(index) {
+                $(this).css("width", $this.find("th").eq(index).css('width'));
+            });
+        } else {
+            $($this.find("tr")[0]).find("td").each(function(index) {
+                $(this).css("width", $headerFixed.find("th").eq(index).css('width'));
+            });
+        }
 
-          if (typeof opts.afterDelete == 'function'){
-            opts.afterDelete.call( $this );
-          }
-
-          break;
-
-        case 'removeAllRows':
-          if (typeof opts.beforeDeleteAll == 'function'){
-            opts.beforeDeleteAll.call( $this );
-          }
-
-          $this.find('tbody tr').remove();
-
-          if ( _isFixedHeaded() ) {
-            _resizeFixedHeader();
-          }
-
-          if (typeof opts.afterDeleteAll == 'function'){
-            opts.afterDeleteAll.call( $this );
-          }
-
-          break;
-      }
+        $this.find("thead").remove();
+        $headerFixed.css('width', $this.width() - _getScrollbarWidth());
+    }
 
     function _fixedHeader() {
         $scrollableFather.wrap('<div id="container-easyTable" />');
@@ -85,25 +63,9 @@
         _resizeFixedHeader();
     }
 
-    function _resizeFixedHeader() {
-
-        if ( $this.find("th").length > 0 ) {
-          $headerFixed.find("th").each(function(index) {
-            $(this).css("width", $this.find("th").eq(index).css('width'));
-          });
-        } else {
-          $($this.find("tr")[0]).find("td").each(function(index) {
-            $(this).css("width", $headerFixed.find("th").eq(index).css('width'));
-          });
-        }
-
-        $this.find("thead").remove();
-        $headerFixed.css('width', $this.width() - _getScrollbarWidth());
-    }
-
     function _removeFixedHeader() {
-      var header = $headerFixed.find('thead');
-        var body = $this.find('tbody');
+        var header = $headerFixed.find('thead'),
+        body = $this.find('tbody');
         $scrollableFather.unwrap();
         header.find('th').each( function () {
             $(this).css('width', '');
@@ -112,61 +74,105 @@
         $headerFixed.remove();
     }
 
-    function _getScrollbarWidth() {
-      if ( $this.height() > $scrollableFather.height() ) {
-        var outer = document.createElement("div");
-        outer.style.visibility = "hidden";
-        outer.style.width = "100px";
-        outer.style.msOverflowStyle = "scrollbar";
-
-        document.body.appendChild(outer);
-
-        var widthNoScroll = outer.offsetWidth;
-
-        outer.style.overflow = "scroll";
-
-        var inner = document.createElement("div");
-        inner.style.width = "100%";
-        outer.appendChild(inner);
-
-        var widthWithScroll = inner.offsetWidth;
-
-        outer.parentNode.removeChild(outer);
-
-        return widthNoScroll - widthWithScroll;
-      }
-      return 0;
-    }
+    
 
     function _addRow() {
-      $newRow = $('<tr>');
+        var $newRow = $('<tr>');
 
-      opts.columnsValues.some( function ( columnValue, index ) {
-        $td = $('<td>');
-        $td.append(columnValue);
-        $newRow.append($td);
+        opts.columnsValues.some( function ( columnValue, index ) {
+            var $td = $('<td>');
+            $td.append(columnValue);
+            $newRow.append($td);
 
-        if ( typeof opts.columnsNames[index] != 'undefined' ) {
-          $td.attr('name', opts.columnsNames[index]);
-        }
+            if ( opts.columnsNames[index] !== undefined ) {
+              $td.attr('name', opts.columnsNames[index]);
+            }
 
-        if ( typeof opts.columnsIDs[index] != 'undefined' ) {
-          $td.attr('id', opts.columnsIDs[index]);
-        }
+            if ( opts.columnsIDs[index] !== undefined ) {
+              $td.attr('id', opts.columnsIDs[index]);
+            }
+        });
 
-      });
-
-      if (typeof opts.animateAdd == 'function') {
+        if (typeof opts.animateAdd === 'function') {
           opts.animateAdd.call( 'undefined' , $this, $newRow );
-      }
+        }
 
-      $this.append($newRow);
+        $this.append($newRow);
 
-      return $newRow;
-    };
+        return $newRow;
+    }
 
     function _isFixedHeaded() {
-      return ( $headerFixed.length > 0 );
+        return ( $headerFixed.length > 0 );
+    }
+
+    switch ( action ) {
+
+        case 'fixedHead':
+            if ( ! _isFixedHeaded() ){
+                _fixedHeader();
+                $(window).resize(_resizeFixedHeader);
+            }
+        break;
+
+        case 'undoFixedHead':
+            if ( _isFixedHeaded() ){
+                _removeFixedHeader();
+            }
+        break;
+
+        case 'addRow':
+            if (typeof opts.beforeAdd === 'function'){
+                opts.beforeAdd.call( $this );
+            }
+
+            $tr = _addRow();
+            if ( _isFixedHeaded() ) {
+                _resizeFixedHeader();
+            }
+
+            if (typeof opts.afterAdd === 'function'){
+                opts.afterAdd.call( $this, $tr );
+            }
+
+        break;
+
+        case 'removeRow':
+            if (typeof opts.beforeDelete === 'function'){
+                opts.beforeDelete.call( $this );
+            }
+
+            opts.indexes.sort().reverse();
+            opts.indexes.some( function ( value ) {
+                $this.find('tbody tr')[value].remove();
+            });
+
+            if ( _isFixedHeaded() ) {
+                _resizeFixedHeader();
+            }
+
+            if (typeof opts.afterDelete === 'function'){
+                opts.afterDelete.call( $this );
+            }
+
+        break;
+
+        case 'removeAllRows':
+            if (typeof opts.beforeDeleteAll === 'function'){
+                opts.beforeDeleteAll.call( $this );
+            }
+
+            $this.find('tbody tr').remove();
+
+            if ( _isFixedHeaded() ) {
+                _resizeFixedHeader();
+            }
+
+            if (typeof opts.afterDeleteAll === 'function'){
+                opts.afterDeleteAll.call( $this );
+            }
+
+        break;
     }
 
     return this;
